567cc85abab6d62ae0f3c0cfd199528881374f73
Mark Dorison <mark@dorison.org>
2014-02-28 16:28:42 -0500

    Example code.

--- UITextFieldContentSizeExample/MCDViewController.m
+++ UITextFieldContentSizeExample/MCDViewController.m
@@ -1,6 +1,6 @@
 //
 //  MCDViewController.m
-//  UITextFieldContentSizeExample
+//  TextViewContentSizeExample
 //
 //  Created by Mark Dorison on 2/28/14.
 //  Copyright (c) 2014 MCD. All rights reserved.
@@ -10,20 +10,32 @@
 
 @interface MCDViewController ()
 
+@property (nonatomic, weak) IBOutlet UITextField *autoResizeTextField;
+
 @end
 
 @implementation MCDViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    [self.autoResizeTextField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)textFieldDidChange:(UITextField *)textField {
+    [UIView animateWithDuration:0.1 animations: ^{
+        NSLog(@"%@", NSStringFromCGSize(textField.intrinsicContentSize));
+
+        // This is triggered correctly on all changes to the UITextField, but seems to have incorrect values
+        [textField invalidateIntrinsicContentSize];
+
+        NSLog(@"%@", NSStringFromCGSize(textField.intrinsicContentSize));
+    }];
 }
 
 @end
